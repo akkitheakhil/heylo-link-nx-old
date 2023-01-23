5d79783926d86588a6947dd7e78142da02764b63
akkitheakhil <speaktoakhilp@gmail.com>
2023-01-23 09:47:39 +0530

    HEYLO-LINK: pwa config and changes

--- apps/heylo-link/next.config.js
+++ apps/heylo-link/next.config.js
@@ -3,23 +3,19 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withNx } = require('@nrwl/next/plugins/with-nx');
 const withPWA = require("next-pwa");
-module.exports = withPWA({
-	pwa: {
-		dest: "public",
-		register: true,
-        disable: process.env.NODE_ENV ===      'development',
-		skipWaiting: true,
-	},
-});
-/**
- * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
- **/
+
 const nextConfig = {
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  pwa: {
+    dest: "public",
+    register: true,
+        disable: process.env.NODE_ENV ===      'development',
+    skipWaiting: true,
+  }
 };
 
-module.exports = withNx(nextConfig);
+module.exports = withNx(withPWA(nextConfig))
